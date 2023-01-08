03785fed7fddca3a6446bd37a191ca046f75bc48
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-01-08 23:27:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function imgslider(image){
+    document.querySelector('.starbucks').src=image;
+}
+function changecirclecolour(color){
+    document.querySelector('.circle').style.background=color;
+}
